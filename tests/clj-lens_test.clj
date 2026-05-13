9feabc54ac97f24ff314a012cd40fe97f61c56cc
Laurence Chen <laurence@replware.dev>
2026-05-13 16:36:13 +0800

    test: add integration tests for all clj-lens modes

---
+++ tests/clj-lens_test.clj
@@ -0,0 +1,43 @@
+(require '[clojure.test :as t]
+         '[cheshire.core :as json]
+         '[clojure.java.shell :as shell])
+
+(defn run-clj-lens [& args]
+  "Run clj-lens.bb and parse JSON output"
+  (let [result (apply shell/sh "bb" "skills/clojure-fast-read/scripts/clj-lens.bb" args)]
+    (if (zero? (:exit result))
+      (json/parse-string (:out result) true)
+      {:status "error" :error "exit-code" :exit (:exit result)})))
+
+(t/deftest test-coordinate-mode
+  (let [response (run-clj-lens "skills/clojure-fast-read/scripts/clj-lens.bb" "1")]
+    (t/is (or (= (:status response) "ok")
+              (= (:status response) "error")))
+    (if (= (:status response) "ok")
+      (t/is (= (:mode response) "coordinate")))))
+
+(t/deftest test-symbol-mode-error
+  (let [response (run-clj-lens "--symbol" "fake.ns/fake")]
+    (t/is (or (= (:status response) "error")
+              (= (:status response) "suggestion")))))
+
+(t/deftest test-find-mode
+  (let [response (run-clj-lens "--find" "zzz")]
+    (t/is (= (:status response) "ok"))
+    (t/is (map? (get response :data)))))
+
+(t/deftest test-last-error-no-nrepl
+  (let [response (run-clj-lens "--last-error")]
+    (t/is (= (:status response) "error"))))
+
+(t/deftest test-trace-mode
+  (let [response (run-clj-lens "--trace" "at app (core.clj:1)")]
+    (t/is (or (= (:status response) "ok")
+              (= (:status response) "error")))))
+
+(t/deftest test-invalid-mode
+  (let [response (run-clj-lens "--invalid")]
+    (t/is (= (:status response) "error"))))
+
+(println "Running clj-lens tests...")
+(t/run-tests)
